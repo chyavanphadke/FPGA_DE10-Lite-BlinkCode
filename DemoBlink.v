641be8ba5a31bb018cc3b58280993f9c0de68611
chyavanphadke <chyavanphadke@gmail.com>
2020-10-10 11:43:42 -0700

    Create DemoBlink.v

---
+++ DemoBlink.v
@@ -0,0 +1,19 @@
+module DemoBlink (
+	input clk,
+	output [9:0] LED
+);
+
+reg [24:0] cnt;
+
+initial begin
+cnt <= 32'h00000000;
+end
+
+always @ (posedge clk) begin
+cnt <= cnt + 1;
+end
+
+	
+assign LED[9:1] = 9'b000000000;
+
+endmodule
